b8feb655043c2595b192e2b78d7f2b8c637ff538
star123-source <goncalves.paula.beatriz@escola.pr.gov.br>
2025-09-09 09:31:12 -0300

    up

--- batatafrita.js
+++ batatafrita.js
@@ -5,4 +5,6 @@ rodri_circulo.addEventListener('dblclick',()=>{
 } )
 rodri_circulo.style.borderRadius = '$(tamanho_rodri)px`
 rodri_circulo.style.width = $(tamanho_rodri) px`
+rodri_circulo.style.heigth = $(tamanho_rodri) px`
 tamanho_rodri = tamanho_rodri+20
+
