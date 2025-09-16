162e7cd8d3dd8549fc5a0aada16d06a57a4289d4
star123-source <goncalves.paula.beatriz@escola.pr.gov.br>
2025-09-16 09:06:43 -0300

    Update batatafrita1.js

--- batatafrita1.js
+++ batatafrita1.js
@@ -17,13 +17,13 @@ if (foto_t== false){
 }
  });
 
-const div = document.getElementById('circulo')
-let div_tamanho  = 1
+const rodri_circulo = document.getElementById('circulo')
+let rodri_tamanho  = 1
 
-div.addEventListener('dbclick' ,() => {
+rodri_circulo.addEventListener('dbclick' ,() => {
  alert (rodri_tamanho)
- rodri_circulo.style.borderRadius = $(rodri_tamanho}px`;
+ rodri_circulo.style.borderRadius =  ´$(rodri_tamanho}px`;
  rodri_circulo.style.width = `$(rodri_tamanho*2)px` ;
 rodri_circulo.style.height = `$(rodri_tamanho*2)px` ;
-
-
+rodri_tamanho += 30;
+});
