a665f0a2380af7ccf3407ccba1251b71bf1c0d7b
star123-source <goncalves.paula.beatriz@escola.pr.gov.br>
2025-09-16 08:24:30 -0300

    Update batatafrita1.js

--- batatafrita1.js
+++ batatafrita1.js
@@ -1,3 +1,5 @@
  Function mudarCor (novaCor) {
    var elemento = document.getElementById ("para1)
    elemento.style.backgroundColor = novaCor
+   elemento.style.backgroundColor = novaCor
+   elemento.style.display
