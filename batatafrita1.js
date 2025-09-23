cdb975fede56896264a66bdf48c4828d7b35db18
star123-source <goncalves.paula.beatriz@escola.pr.gov.br>
2025-09-23 08:36:26 -0300

    up

--- batatafrita1.js
+++ batatafrita1.js
@@ -33,23 +33,22 @@ rodri_circulo.addEventListener('dblclick', () => {
 });
 
 // Validação de idade
-const form = document.getElementById("texto-form");
+const form = document.getElementById("texto");
 const section = document.getElementById("sc");
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", (event) =>{
   event.preventDefault();
-  const input = form.querySelector("input");
-  const valor = parseInt(input.value);
-  const p = section.querySelector('p');
-
-  if (isNaN(valor) || valor < 24) {
-    p.innerText = "Professor é mais velho";
-    p.style.display = 'block';
-
-  } else {
-
-    p.innerHTML = '<h1>Olá, Mundo</h1>';
-    p.style.display = 'block';
+  var input = form.querySelector('input');
+  if (parseInt(input.value)) < 24 || isNaN(input.value) ){
+  var p = section.querySelector ('p')
+  p.innerText = "Professor é mais velho";
+  p.style.display = 'block';
+  }else{
+  
+  var p = section.querySelector('p')
+  p.innerHTML = '<p> Olá, Mundo <h1>'
+  p.style.display = 'valor'
   }
-});
+
+  
 
